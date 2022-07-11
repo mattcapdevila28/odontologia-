eb00cd8142c8af1904a8bbd8d3fd9a770b041617
Matias Capdevila <tripodi71@hotmail.com>
2022-07-11 00:14:51 -0300

    cambio para js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let nombreUsuario = prompt("Ingresá tu correo para recibir información y promociones")
+alert("Bienvenido/a, gracias por confiar en nosotros!")
+
+let cupon = prompt('Si posee un cupón, ingreselo.');
+let cuponValido = 'UALICIDAD';
+if (cupon == 'UALICIDAD' || cupon == 'Ualicidad' || cupon == 'ualicidad') {
+    alert('Cupón ingresado con exito!');
+} else {
+    alert('Cupón inválido.');
+} 
+let frase = 12;
+while (frase>0){console.log('Boquita el mas grande')
+frase--;
+}
+console.log("Y nadie lo puede negar");
+    
