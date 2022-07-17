14227575997411b33ea333ca972c418d9d6d20ed
Matias Capdevila <tripodi71@hotmail.com>
2022-07-16 21:43:02 -0300

    ultimo cambio js

--- js/script.js
+++ js/script.js
@@ -13,4 +13,44 @@ while (frase>0){console.log('Boquita el mas grande')
 frase--;
 }
 console.log("Y nadie lo puede negar");
-    
+
+const doctor1 = {
+    nombreYapellido: 'Doctor Sebastiano Lozano',
+    calle: 'avenida caseros 3547',
+    edad: 24,
+    esSoltero: true,
+    dias: 'Lunes',
+}
+
+
+const doctor2 = {
+    nombreYapellido: 'Doctor Yao Ming',
+    calle: 'avenida Jujuy 550',
+    edad: 38,
+    esSoltera: false,
+    dias: 'miercoles',
+}
+
+const doctor3 = {
+    nombreYapellido: 'Doctor Dario Chaile',
+    calle: 'avenida La plata 751',
+    edad: 34,
+    esSoltero: true,
+    dias: 'viernes',
+}
+
+function Doctor(nombreYapellido, calle, edad, dias) {
+    this.nombreYapellido = nombreYapellido;
+    this.calle = calle;
+    this.edad = edad;
+    this.dias = dias;
+}
+
+alert("Si buscas formar parte de nuestro equipo, ingresa tus datos")    
+let nombreYapellidoDoctor = prompt('Ingresa tú nombre y apellido');
+let edadDoctor = prompt('Ingresa tú edad');
+let diasDoctor = prompt('Ingresa tus días disponibles');
+
+const DoctorUsuario = new Doctor(nombreYapellidoDoctor,edadDoctor,diasDoctor);
+
+console.log(DoctorUsuario)
