cbd25a7ad3b8270b13e531cf39f3ed97a635d0a9
Matias Capdevila <tripodi71@hotmail.com>
2022-08-16 23:01:34 -0300

    libreria

--- js/blanqueamiento.js
+++ js/blanqueamiento.js
@@ -4,13 +4,14 @@ btnSwal.onclick = () => {
     Swal.fire(
         {
             title: '¡Hola!',
-            text: 'Te damos la bienvenida a nuestra app',
+            text: '¡Nunca es tarde para tener la sonrisa perfecta que deseas!',
             icon: 'info',
-            iconColor: '#66f4ae',
-            confirmButtonText: 'Gracias',
+            iconColor: '#76e5fd',
+            confirmButtonText: '¡Si quiero!',
             showCancelButton: true,
-            cancelButtonText: 'No me interesa',
-            timer: 2500
+            cancelButtonText: 'No me interesa.',
+            color: '#6a9ddd',
+            timer: 3500
         }
     )
 }
