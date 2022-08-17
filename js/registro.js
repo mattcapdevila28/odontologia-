7cfba30aeb63644c310dce89f5db4a5d90ec16cb
Matias Capdevila <tripodi71@hotmail.com>
2022-08-16 23:42:49 -0300

    libreria

--- js/registro.js
+++ js/registro.js
@@ -19,7 +19,22 @@ let inputState = document.getElementById('inputState');
 let inputZip = document.getElementById('inputZip');
 
 function completarRegistro() {
-    alert("Gracias por registrarte!");
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Signed in successfully'
+      });
 }
 
 function derechosReservados() {
