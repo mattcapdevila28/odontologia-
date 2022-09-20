5cafba833e5ce1df13fff7b0493547520f174480
Matias Capdevila <tripodi71@hotmail.com>
2022-09-20 07:22:35 -0300

    ultimo

--- js/registro.js
+++ js/registro.js
@@ -1,50 +1,24 @@
 const btnRegistro = document.getElementById('registro');
-const btnGuardar = document.getElementById('gridCheck')
+const btnGuardar = document.getElementById('gridCheck');
+const email = document.getElementById('inputEmail4');
 let inputEmail4 = document.getElementById('inputEmail4');
 let inputPassword4 = document.getElementById('inputPassword4');
-let inputAddress = document.getElementById('inputAddress');
-let inputAddress2 = document.getElementById('inputAddress2');
-let inputCity = document.getElementById('inputCity');
-let inputState = document.getElementById('inputState');
-let inputZip = document.getElementById('inputZip');
 
-//form
-inputEmail4.addEventListener('keyup',()=>{
-    console.log(inputEmail4.value);
-})
-
-inputPassword4.addEventListener('keyup',()=>{
-    console.log(inputPassword4.value);
-})
-
-inputAddress.addEventListener('keyup',()=>{
-    console.log(inputAddress.value);
-})
-
-inputAddress2.addEventListener('keyup',()=>{
-    console.log(inputAddress2.value);
-})
-
-inputCity.addEventListener('keyup',()=>{
-    console.log(inputCity.value);
-})
-
-inputState.addEventListener('keyup',()=>{
-    console.log(inputState.value);
-})
-
-inputZip.addEventListener('keyup',()=>{
-    console.log(inputZip.value);
-})
-
+    
 // alert de registro
 btnRegistro.onclick = () => {
+    e.preventDefault();
     Swal.fire({
         position: 'top-end',
         icon: 'success',
         title: 'Registro exitoso!',
         showConfirmButton: true,
         timer: 3500
+    },
+        function (isConfirm) {
+            if (isConfirm) {
+                return true;
+            }
       });
 }
 
@@ -62,7 +36,7 @@ function guardarDatos(storage) {
         sessionStorage.setItem('inputEmail4', JSON.stringify(email));
     }
 }
-btnGuardar.addEventListener('click',()=>{
+btnRegistro.addEventListener('click',()=>{
     if (btnGuardar.checked) {
         guardarDatos('localStorage');
     } else {
@@ -72,13 +46,13 @@ btnGuardar.addEventListener('click',()=>{
 
 // api
 async function verificacionEmail (inputEmail4) {
-let API =  ` https://www.disify.com/api/email/${inputEmail4}`;
+let API =  `{inputEmail4}`;
 const resp = await fetch(API);
 const dataJson = await resp.json();
 console.log(dataJson);
 }
 
-btnRegistro.addEventListener('click',()=>{
-e.preventDefault();
+btnRegistro.addEventListener("click", function(event){
+    event.preventDefault();
 verificacionEmail(inputEmail4)
 })
